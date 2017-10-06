6a6edc2644bd34bdcb7c6e2ab84eb5fd3842ed23
sankettps <sanket.tps@gmail.com>
2017-10-06 20:22:50 +0530

    template added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,5 +11,7 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require turbolinks
-//= require_tree .
+//= require jquery
+//= require bootstrap
+//= require metisMenu
+//= require sb-admin-2
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
-class HomeController < ShopifyApp::AuthenticatedController
+# class HomeController < ShopifyApp::AuthenticatedController
+class HomeController < ApplicationController
   def index
-    @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
-    @webhooks = ShopifyAPI::Webhook.find(:all)
+    # @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
+    # @webhooks = ShopifyAPI::Webhook.find(:all)
   end
 end
