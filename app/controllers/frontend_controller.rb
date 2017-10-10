c141beea6719aa7a60d6192bd4cc2f30a2ee2caf
sankettps <sanket.tps@gmail.com>
2017-10-10 21:44:50 +0530

    dashboard and frontend controller added

---
+++ app/controllers/frontend_controller.rb
@@ -0,0 +1,10 @@
+class FrontendController < ApplicationController
+	def get_upsell_detail
+		# $.getScript( "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js", function( ) {
+  #     $("#test").click(function() {
+		# 		console.log(typeof $().modal == 'function')
+  #       $('#myModal').modal('show');
+		# 	});
+  # 	})
+	end
+end
