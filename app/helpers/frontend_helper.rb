8b63f32e66d087323251992e7ebc27c48110730f
sankettps <sanket.tps@gmail.com>
2017-10-12 07:36:20 +0530

    logic added for open modal in shop.rb, nethod added for create html in helper

--- app/helpers/frontend_helper.rb
+++ app/helpers/frontend_helper.rb
@@ -1,2 +1,21 @@
 module FrontendHelper
+	def modal_html
+		@html = "<div id=\"hfUpsell\" class=\"modal fade\" role=\"dialog\">
+			  <div class=\"modal-dialog\">
+			    <!-- Modal content-->
+			    <div class=\"modal-content\">
+			      <div class=\"modal-header\">
+			        <h4 class=\"modal-title\">#{@funnel.name}</h4>
+			      </div>
+			      <div class=\"modal-body\">
+			        <p>#{@funnel.up_product.title}</p>
+			      </div>
+			      <div class=\"modal-footer\">
+			        <button type=\"button\" class=\"btn btn-default\" data-dismiss=\"modal\">Close</button>
+			      </div>
+			    </div>
+
+			  </div>
+			</div>"
+	end
 end
