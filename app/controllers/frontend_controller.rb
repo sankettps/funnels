b09e44b4d7cab30923de838d0ba2aefb23509799
sankettps <sanket.tps@gmail.com>
2017-10-14 21:26:57 +0530

    add private app url to frontend controller

--- app/controllers/frontend_controller.rb
+++ app/controllers/frontend_controller.rb
@@ -7,8 +7,10 @@ class FrontendController < ApplicationController
 	 		@filter_product = FilterShopProduct.find_by(product_id: params[:product_id])
 	 		@funnel = @filter_product.funnels.find_by(is_active: true) if @filter_product
 
-	 		@shop_url ="https://#{ShopifyApp.configuration.api_key}:#{@shop.shopify_token}@#{@shop.shopify_domain}/admin/"
+	 		# @shop_url ="https://#{ShopifyApp.configuration.api_key}:#{@shop.shopify_token}@#{@shop.shopify_domain}/admin/"
+  	@shop_url = "https://fd7ec4c589db58b5652eccf59279b7d3:520600ed3d4e5b15de332ab367f25ea8@welovedrones.myshopify.com/admin/"
     	ShopifyAPI::Base.site = @shop_url
+		 		puts "<======funnel========#{@funnel.inspect}===============>"
     	if @funnel
 		 		@up_product = ShopifyAPI::Product.find(@funnel.up_product_id)
     		@up_variant = @up_product.variants.first
