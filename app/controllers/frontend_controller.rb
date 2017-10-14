a7fda4a63ab4266d2dbe55d3973553b26f348f3e
sankettps <sanket.tps@gmail.com>
2017-10-14 21:31:04 +0530

    product id changed

--- app/controllers/frontend_controller.rb
+++ app/controllers/frontend_controller.rb
@@ -7,15 +7,15 @@ class FrontendController < ApplicationController
 	 		@filter_product = FilterShopProduct.find_by(product_id: params[:product_id])
 	 		@funnel = @filter_product.funnels.find_by(is_active: true) if @filter_product
 
-	 		# @shop_url ="https://#{ShopifyApp.configuration.api_key}:#{@shop.shopify_token}@#{@shop.shopify_domain}/admin/"
-  	@shop_url = "https://fd7ec4c589db58b5652eccf59279b7d3:520600ed3d4e5b15de332ab367f25ea8@welovedrones.myshopify.com/admin/"
+	 		@shop_url ="https://#{ShopifyApp.configuration.api_key}:#{@shop.shopify_token}@#{@shop.shopify_domain}/admin/"
+  	# @shop_url = "https://fd7ec4c589db58b5652eccf59279b7d3:520600ed3d4e5b15de332ab367f25ea8@welovedrones.myshopify.com/admin/"
     	ShopifyAPI::Base.site = @shop_url
 		 		puts "<======funnel========#{@funnel.inspect}===============>"
     	if @funnel
-		 		@up_product = ShopifyAPI::Product.find(@funnel.up_product_id)
+		 		@up_product = ShopifyAPI::Product.find(@funnel.up_product.product_id)
     		@up_variant = @up_product.variants.first
 		 		
-		 		@down_product = ShopifyAPI::Product.find(@funnel.down_product_id)
+		 		@down_product = ShopifyAPI::Product.find(@funnel.down_product.product_id)
     		@down_variant = @down_product.variants.first
 		 		puts "<======test product========#{@up_product.inspect}===============>"
 		 		modal_html
