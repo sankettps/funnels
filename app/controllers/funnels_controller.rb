7ad365b7cc837f2bb98cbde9e3930d808830c954
sankettps <sanket.tps@gmail.com>
2017-10-16 23:02:11 +0530

    funnel create logic added to funnel controller

--- app/controllers/funnels_controller.rb
+++ app/controllers/funnels_controller.rb
@@ -1,4 +1,6 @@
-class FunnelsController < ApplicationController
+class FunnelsController < ShopifyApp::AuthenticatedController
+# class FunnelsController < ApplicationController
+  before_action :set_current_shop
   before_action :set_funnel, only: [:show, :edit, :update, :destroy]
 
   # GET /funnels
@@ -24,11 +26,19 @@ class FunnelsController < ApplicationController
   # POST /funnels
   # POST /funnels.json
   def create
-    @funnel = Funnel.new(funnel_params)
-
+    # @funnel = Funnel.new(funnel_params)
+    params[:funnel][:up_product_id] = FilterShopProduct.find_by(product_id: params[:funnel][:up_product_id]).try(:id)
+    params[:funnel][:down_product_id] = FilterShopProduct.find_by(product_id: params[:funnel][:down_product_id]).try(:id)
+    @funnel = @shop.funnels.create(funnel_params)
+    if @funnel.present?
+      funnel_product_ids = FilterShopProduct.where(product_id: params[funnel][funnel_product_ids].split(',')).ids
+      @funnel.filter_shop_product_ids = funnel_product_ids if funnel_product_ids.present?
+      # Funnel.first.filter_shop_product_ids = [1, 2, 3, 4, 5]
+      # params[funnel][funnel_product_ids]
+    end
     respond_to do |format|
       if @funnel.save
-        format.html { redirect_to @funnel, notice: 'Funnel was successfully created.' }
+        format.html { redirect_to root_path, notice: 'Funnel was successfully created.' }
         format.json { render :show, status: :created, location: @funnel }
       else
         format.html { render :new }
@@ -67,8 +77,14 @@ class FunnelsController < ApplicationController
       @funnel = Funnel.find(params[:id])
     end
 
+    def set_current_shop
+      @current_shop = ShopifyAPI::Shop.current
+      @shop = Shop.find_by_shopify_domain(@current_shop.myshopify_domain)
+    # @shop = Shop.first
+    end
+
     # Never trust parameters from the scary internet, only allow the white list through.
     def funnel_params
-      params.require(:funnel).permit(:name, :up_product_id, :down_product_id, :is_active, :shop_id)
+      params.require(:funnel).permit(:name, :title :up_product_id, :down_product_id, :is_active, :shop_id)
     end
 end
