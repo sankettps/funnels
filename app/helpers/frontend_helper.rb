b0a12c72986712b3f1c377fc031fc56c3049944c
Piyush Ghevariya <piyushkghevariya@gmail.com>
2017-10-30 21:56:21 +0530

    chnag in updell js

--- app/helpers/frontend_helper.rb
+++ app/helpers/frontend_helper.rb
@@ -111,16 +111,7 @@ module FrontendHelper
 		  </div>
 		</div>
 		<script>
-			var selectUpsellCallback = function(variant, selector) {
-		        console.log(variant);
-		        $('#hfUpsellVariant').val(variant.id);
-		     };
-
-			this.optionSelector = new Shopify.OptionSelectors('herofunnelProduct', {
-		        product: #{@up_product.to_json},
-		        onVariantSelected: selectUpsellCallback,
-		        enableHistoryState: this.enableHistoryState
-		      });
+			
 		</script>
 		<style type=\"text/css\">
 			.hf-upsell .hf-pro-img{
