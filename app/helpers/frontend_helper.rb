211b3cabbd056647c443e2d4b9bbd665fdd6d059
Piyush Ghevariya <piyushkghevariya@gmail.com>
2017-10-31 23:48:27 +0530

    change in updell js

--- app/helpers/frontend_helper.rb
+++ app/helpers/frontend_helper.rb
@@ -58,15 +58,15 @@ module FrontendHelper
 										</div>
 						     <div>
 				      </div>
-				      <div class=\"row\">
-		    				<div class=\"col-xs-12\">
-		      				<div class=\"hf-pro-desc\">
-		      					#{@up_product.body_html.html_safe}
-		      				</div>
-		    				</div>
-		    			</div>
 				    </div>
 		      </div>
+		      <div class=\"row upSellDes\">
+    				<div class=\"col-xs-12\">
+      				<div class=\"hf-pro-desc\">
+      					#{@up_product.body_html.html_safe}
+      				</div>
+    				</div>
+    			</div>
 		      <div id=\"hfDownsellBody\" style=\"display:none;\">
 			      <div class=\"modal-body\">
 				      <div class=\"row\">
@@ -95,13 +95,6 @@ module FrontendHelper
 										</div>
 						     <div>
 				      </div>
-				      <div class=\"row\">
-		    				<div class=\"col-xs-12\">
-		      				<div class=\"hf-pro-desc\">
-		      					#{@down_product.body_html.html_safe}
-		      				</div>
-		    				</div>
-		    			</div>
 				    </div>
 			      <div class=\"modal-footer\">
 			        <button type=\"button\" class=\"btn btn-success\" id=\"hfDownsellBuy\">Buy Now</button>
@@ -109,7 +102,13 @@ module FrontendHelper
 			      </div>
 		      </div>
 
-		      
+		      <div class=\"row downSellDes\">
+    				<div class=\"col-xs-12\">
+      				<div class=\"hf-pro-desc\">
+      					#{@down_product.body_html.html_safe}
+      				</div>
+    				</div>
+    			</div>
 		    </div>
 		  </div>
 		  <div class=\"modal-footer\">
