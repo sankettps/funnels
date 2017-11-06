1cba22c2d25f1235cac817b8ef709fcda9abff5c
sankettps <sanket.tps@gmail.com>
2017-11-06 22:18:47 +0530

    field name change

--- db/migrate/20171103165810_create_funnel_reports.rb
+++ db/migrate/20171103165810_create_funnel_reports.rb
@@ -4,7 +4,7 @@ class CreateFunnelReports < ActiveRecord::Migration[5.1]
       t.string :product_id
       t.string :up_product_id
       t.string :down_product_id
-      t.string :action
+      t.string :hf_action
       t.float :price
       t.references :shop, foreign_key: true
 
