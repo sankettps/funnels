42a0e29566e21e309755912e99ed199da81438b0
Piyush Ghevariya <piyushkghevariya@gmail.com>
2017-11-09 22:23:27 +0530

    add console

--- app/helpers/frontend_helper.rb
+++ app/helpers/frontend_helper.rb
@@ -1,23 +1,4 @@
 module FrontendHelper
-	def modal_html1
-		@html = "<div id=\"hfUpsellModal\" class=\"modal fade\" role=\"dialog\">
-			  <div class=\"modal-dialog\">
-			    <!-- Modal content-->
-			    <div class=\"modal-content\">
-			      <div class=\"modal-header\">
-			        <h4 class=\"modal-title\">#{@funnel.name}</h4>
-			      </div>
-			      <div class=\"modal-body\">
-			        <p>#{@funnel.up_product.title}</p>
-			      </div>
-			      <div class=\"modal-footer\">
-			        <button type=\"button\" class=\"btn btn-default\" data-dismiss=\"modal\">Close</button>
-			      </div>
-			    </div>
-
-			  </div>
-			</div>"
-	end
 	
 	def modal_html
 		@html = "<div id=\"hfUpsellModal\" class=\"modal fade hf-upsell\" role=\"dialog\">
