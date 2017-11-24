b8051fe2cd92c181d0be9ffae14c80380927061c
Piyush Ghevariya <piyushkghevariya@gmail.com>
2017-11-24 22:52:07 +0530

    change in downsell popup html

--- app/controllers/frontend_controller.rb
+++ app/controllers/frontend_controller.rb
@@ -74,7 +74,7 @@ class FrontendController < ApplicationController
     	if @funnel
     		puts "Innnnnnnnnnnnnnnnn"
     		
-		 		@html = downsell_modal_html
+		 		@html = downsell_modal_html_piyush #downsell_modal_html
 		 		# @funnel
 		 		@response = {data: @html,hf_time_out:(@funnel.down_sell_time_out * 1000),track_id: @funnel.id}
 		 	else
