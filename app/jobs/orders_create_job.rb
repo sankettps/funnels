376f7cb1b2c47dd79662ee7c31990adae7c9512c
sankettps <sanket.tps@gmail.com>
2017-11-30 21:02:05 +0530

    webhook call altered

--- app/jobs/orders_create_job.rb
+++ app/jobs/orders_create_job.rb
@@ -9,10 +9,10 @@ class OrdersCreateJob < ActiveJob::Base
     	FunnelReport.where(cart_token: cart_token).each do |report|
     		# webhook['line_items']any? {|h| h[:variant_id] == report.product_id}
     		puts "report found----->#{report.inspect}"
-    		line_items = webhook['line_items'].group_by { |x| x[:variant_id] }
+    		line_items = webhook['line_items'].group_by { |x| x["variant_id"] }
 				line_item = line_items[report.product_id]
     		puts "line item there----->#{line_item.inspect}"
-    		report.update(price: line_item["price"],is_purchased: true) if line_item.present?
+    		report.update(price: line_item[0]["price"],is_purchased: true) if line_item.present?
     	end
     end
   end
