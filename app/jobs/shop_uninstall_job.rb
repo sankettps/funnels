df4e788e27e136cb9c7a0df6efac98b22025f0ca
Piyush Ghevariya <piyushkghevariya@gmail.com>
2018-01-06 13:37:08 +0530

    change in shop_uninstall job

--- app/jobs/shop_uninstall_job.rb
+++ app/jobs/shop_uninstall_job.rb
@@ -6,7 +6,6 @@ class ShopUninstallJob < ActiveJob::Base
     shop.with_shopify_session do
     	shop.filter_shop_attributes.destroy_all
     	shop.filter_shop_products.destroy_all
-    	Offer.where(store_id: shop.uuid).destroy_all
     	shop.update_attributes(is_deleted: true,is_products_cloned: false,is_products_clonning: false)
     end
   end
