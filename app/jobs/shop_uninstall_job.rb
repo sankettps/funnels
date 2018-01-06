9814507b8df95dc7cb3919bd37727eeb3088fc03
Piyush Ghevariya <piyushkghevariya@gmail.com>
2018-01-06 13:30:04 +0530

    change in shop_uninstall job

--- app/jobs/shop_uninstall_job.rb
+++ app/jobs/shop_uninstall_job.rb
@@ -8,18 +8,6 @@ class ShopUninstallJob < ActiveJob::Base
     	shop.filter_shop_products.destroy_all
     	Offer.where(store_id: shop.uuid).destroy_all
     	shop.update_attributes(is_deleted: true,is_products_cloned: false,is_products_clonning: false)
-
-    	# install / uninstall track in mautic
-    	url = URI("https://e.hulkcode.com/mautic-contacts/shopifyapp.php")
-		http = Net::HTTP.new(url.host, url.port)
-		http.use_ssl = true
-		http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-		request = Net::HTTP::Post.new(url)
-		request["content-type"] = 'application/json'
-		request["cache-control"] = 'no-cache'
-		request.body = '{"name": "'+webhook['name']+'","email": "'+webhook['email']+'","address1": "'+webhook['address1']+'","city": "'+webhook['city']+'","country": "'+webhook['country_name']+'","province": "'+webhook['province']+'","zip": "'+webhook['zip']+'","phone": "'+webhook['phone']+'","appcode": "volumediscount","status": "uninstall"}'
-
-		response = http.request(request)
     end
   end
 end
