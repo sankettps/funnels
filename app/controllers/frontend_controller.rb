0943f65e67af09d0375099926531ce834d2bdcad
Piyush Ghevariya <piyushkghevariya@gmail.com>
2018-01-08 22:42:18 +0530

    change in frontend helper

--- app/controllers/frontend_controller.rb
+++ app/controllers/frontend_controller.rb
@@ -97,8 +97,8 @@ class FrontendController < ApplicationController
 	 		@filter_product = FilterShopProduct.find_by(product_id: params[:product_id])
 	 		@funnel = @filter_product.funnels.find_by(is_active: true) if @filter_product
 
-	 		@shop_url ="https://#{ShopifyApp.configuration.api_key}:#{@shop.shopify_token}@#{@shop.shopify_domain}/admin/"
-  		# @shop_url = "https://fd7ec4c589db58b5652eccf59279b7d3:520600ed3d4e5b15de332ab367f25ea8@welovedrones.myshopify.com/admin/"
+	 		# @shop_url ="https://#{ShopifyApp.configuration.api_key}:#{@shop.shopify_token}@#{@shop.shopify_domain}/admin/"
+  		@shop_url = "https://fd7ec4c589db58b5652eccf59279b7d3:520600ed3d4e5b15de332ab367f25ea8@welovedrones.myshopify.com/admin/"
     	ShopifyAPI::Base.site = @shop_url
 			puts "<======funnel========#{@funnel.inspect}===============>"
     	if @funnel
